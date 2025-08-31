ffb31b8fb78ff1722473b93d4d47f7c282e83956
Sara Baqla <missatrox44@gmail.com>
2025-08-31 14:47:22 -0500

    styled theme button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
       {/* <Text style={styles.text}>Home screen</Text> */}
