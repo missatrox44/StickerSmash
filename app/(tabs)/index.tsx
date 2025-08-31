00f104b919de7a429c7f12336741f942a9579868
Sara Baqla <missatrox44@gmail.com>
2025-08-31 14:32:56 -0500

    image viewer placeholder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import { Text, View, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+import { Link } from 'expo-router';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      {/* <Text style={styles.text}>Home screen</Text> */}
       {/* <Link href="/about" style={styles.button}>
         Go to About screen
       </Link> */}
@@ -12,19 +19,23 @@ export default function Index() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  },
+  // text: {
+  //   color: '#fff',
+  // },
+  // button: {
+  //   fontSize: 20,
+  //   textDecorationLine: 'underline',
+  //   color: '#fff',
+  // },
 });
