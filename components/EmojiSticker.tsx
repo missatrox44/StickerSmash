2f1e1e471058573cd1f5befc9eb115edd05cab4a
Sara Baqla <missatrox44@gmail.com>
2025-08-31 19:42:20 -0500

    pan gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -10,6 +10,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -22,6 +24,7 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     });
 
 
+  // translation values defineed will move the sticker around the screen
   const imageStyle = useAnimatedStyle(() => {
     return {
       width: withSpring(scaleImage.value),
@@ -29,17 +32,39 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     };
   });
 
+  const drag = Gesture.Pan().onChange(event => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
 
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-        {/* <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} /> */}
-      </GestureDetector>
-    </View>
+    // <View style={{ top: -350 }}>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+          {/* <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} /> */}
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
+    // </View>
   );
 }
